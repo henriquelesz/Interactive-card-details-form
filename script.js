ccaca965dc128ea5b991deaf82a5cc51d4ffcaa0
henriquelesz <131972114+henriquelesz@users.noreply.github.com>
2024-01-29 17:12:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,24 @@
-// Primeira tentativa
+// Terceira tentativa
 
-/* function updateCard() {
-  const cardNameInput = document.querySelector('#cardNameInput').value;
-  const cardNumberInput = document.querySelector('#cardNumberInput').value;
-  const monthExpInput = document.querySelector('#expMonthInput').value;
-  const yearExpInput = document.querySelector('#expYearInput').value;
-  const cvcInput = document.querySelector('#cvcNumberInput').value;
+  // Selecionar todos os valores dos inputs e atualizar no cartão
 
-  document.querySelector('#cardName').innerHTML = cardNameInput;
-  document.querySelector('#cardNumber').innerHTML = cardNumberInput;
-  document.querySelector('#cardExp').innerHTML = `${monthExpInput}/${yearExpInput}`;
-  document.querySelector('#cardCVC').innerHTML = cvcInput;
-}
-
-document.querySelectorAll('input').forEach((input) => {
-  input.addEventListener('input', updateCard)
-}) */
-
-// Segunda tentativa
-
-  // Atualizar Nome no Card
-
-const cardNameInput = document.querySelector("#cardNameInput");
-
-cardNameInput.addEventListener("input", updateCardName);
-
-function updateCardName() {
-  const cardNameInputValue = document.querySelector("#cardNameInput").value;
-
-  document.querySelector("#cardName").innerHTML = cardNameInputValue;
-}
-
-  // Atualizar Número no Card
-
-const cardNumberInput = document.querySelector("#cardNumberInput");
-
-cardNumberInput.addEventListener("input", updateCardNumber);
-
-function updateCardNumber() {
-  const cardNumberInputValue = document.querySelector('#cardNumberInput').value;
-
-  document.querySelector('#cardNumber').innerHTML = cardNumberInputValue;
-}
-
-
-  // Atualizar CVC no Card
-
-const cvcNumberInput = document.querySelector('#cvcNumberInput');
-
-cvcNumberInput.addEventListener('input', updateCardCVC);
-
-function updateCardCVC() {
-  const cvcNumberInputValue = document.querySelector('#cvcNumberInput').value;
-
-  document.querySelector('#cardCVC').innerHTML = cvcNumberInputValue;
-}
-
+  function updateCard() {
+    const cardNameInputValue = document.querySelector("#cardNameInput").value;
+    const cardNumberInputValue = document.querySelector('#cardNumberInput').value.padEnd(16, '0');
+    const cvcNumberInputValue = document.querySelector('#cvcNumberInput').value;
+    let expMonthInputValue = document.querySelector('#expMonthInput').value.padStart(2, 0);
+    let expYearInputValue = document.querySelector('#expYearInput').value.padStart(2, 0);
   
+    document.querySelector("#cardName").innerHTML = cardNameInputValue.toUpperCase();
+    document.querySelector('#cardNumber').innerHTML = `${cardNumberInputValue.slice(0, 4).padEnd(4, '0')} ${cardNumberInputValue.slice(4, 8).padEnd(4, '0')} ${cardNumberInputValue.slice(8, 12).padEnd(4, '0')} ${cardNumberInputValue.slice(12, 16).padEnd(4, '0')}`;
+    document.querySelector('#cardCVC').innerHTML = cvcNumberInputValue;
+    document.querySelector('#cardExp').innerHTML = `${expMonthInputValue}/${expYearInputValue}`;
+  }
 
-  // Atualizar EXP no Card
+  // Selecionar todos os inputs
 
-const expMonthInput = document.querySelector('#expMonthInput');
-const expYeahInput = document.querySelector('#expYearInput');
+  const cardInputs = document.querySelectorAll('input');
 
-expMonthInput.addEventListener('input', updateCardExp);
-expYearInput.addEventListener('input', updateCardExp);
-
-
-function updateCardExp() {
-  let expMonthInputValue = document.querySelector('#expMonthInput').value;
-  let expYearInputValue = document.querySelector('#expYearInput').value;
-
-  document.querySelector('#cardExp').innerHTML = `${expMonthInputValue}/${expYearInputValue}`;
-}
+   cardInputs.forEach((input) => {
+    input.addEventListener('input', updateCard)
+  });
